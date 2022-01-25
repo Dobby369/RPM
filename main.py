8d4d350275d48b60a81a14de5f032af03631fb9b
Tomogochi <malkova.ol939@gmail.com>
2022-01-25 23:37:04 +0300

    Решение квадратных уравнений

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-if __name__ == '__main__':
-    print('hello world')
+import math
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+D = b ** 2 - 4 * a * c
+if D < 0:
+    print("Корней нет")
+elif D == 0:
+    x = -b / 2 * a
+    print (x)
+else:
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
+    print("x1= ", x1)
+    print("x2= ", x2)
